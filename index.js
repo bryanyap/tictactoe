fafc41d08f1a93ad2a41b5002f326d5abf9aaba6
Edison Neo <edineo62@MacEddie.local>
2017-07-01 23:11:34 +0800

    Remove sass middleware

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ const sass = require('node-sass')
 app.set('view engine', 'pug')
 
 
-app.use(
-	 sass.middleware({
-	     src: __dirname + '/src/sass', //where the sass files are 
-	     dest: __dirname + '/dists/css', //where css should go
-	     debug: true // obvious
-	 })
- );
-
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
