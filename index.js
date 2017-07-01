c909cda8305ed1b7c39e35b830bbcb85c2067519
Edison Neo <edineo62@MacEddie.local>
2017-07-01 23:09:37 +0800

    Install sass

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
-
+const sass = require('node-sass')
 app.set('view engine', 'pug')
 
+
+app.use(
+	 sass.middleware({
+	     src: __dirname + '/src/sass', //where the sass files are 
+	     dest: __dirname + '/dists/css', //where css should go
+	     debug: true // obvious
+	 })
+ );
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
+
+
 
 
 
